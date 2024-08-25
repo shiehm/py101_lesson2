5c6d4dde9423e38667c84d8f4bfcd0ea70b6476f
Melvin Shieh <shieh.melvin@gmail.com>
2024-08-25 19:33:51 +0000

    added 5 game feature to spock_lizard.py

--- lesson_2/spock_lizard.py
+++ lesson_2/spock_lizard.py
@@ -10,44 +10,84 @@ Rock Paper Scissor Spock Lizard Rules:
 import random
 
 VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard']
+SHORT_CHOICES = ['r', 'p', 'sc', 'sp', 'l']
 
 def prompt(msg):
     print(f'-> {msg}')
-    
+
 def display_winner(user_choice, computer_choice):
     if (
-        (user_choice == 'rock' and computer_choice in ['lizard', 'scissors']) or
-        (user_choice == 'paper' and computer_choice in ['rock', 'spock']) or
-        (user_choice == 'scissor' and computer_choice in ['paper', 'lizard']) or
-        (user_choice == 'spock' and computer_choice in ['rock', 'scissors']) or
-        (user_choice == 'lizard' and computer_choice in ['paper', 'spock']) 
+        (user_choice[0] == 'r' and computer_choice in ['lizard', 'scissors']) or
+        (user_choice[0] == 'p' and computer_choice in ['rock', 'spock']) or
+        (user_choice[0:2] == 'sc' and computer_choice in ['paper', 'lizard']) or
+        (user_choice[0:2] == 'sp' and computer_choice in ['rock', 'scissors']) or
+        (user_choice[0] == 'l' and computer_choice in ['paper', 'spock'])
         ):
-        prompt('You Win!')
-    elif user_choice == computer_choice:
-        prompt('Tie!')
+        prompt('You win this round!')
+    elif (
+        (user_choice[0] == 'r' and computer_choice in ['paper', 'spock']) or
+        (user_choice[0] == 'p' and computer_choice in ['lizard', 'scissors']) or
+        (user_choice[0:2] == 'sc' and computer_choice in ['rock', 'spock']) or
+        (user_choice[0:2] == 'sp' and computer_choice in ['lizard', 'paper']) or
+        (user_choice[0] == 'l' and computer_choice in ['rock', 'scissors'])
+        ):
+        prompt('You lose this round!')
     else:
-        prompt('You Lose!')
+        prompt('Tie!')
 
-while True:
+prompt('This is Rock, Paper, Scissors, Spock, Lizard. Best out of 5.')
+
+computer_won = 0
+user_won = 0
+
+while (user_won < 3) or (computer_won < 3):
     prompt(f"Choose from: {', '.join(VALID_CHOICES)}")
-    user_choice = input() 
-    
-    while user_choice not in VALID_CHOICES:
+    user_choice = input()
+
+    while (user_choice not in VALID_CHOICES) and (user_choice not in SHORT_CHOICES):
         prompt(f"That was not a valid choice, choose from: {', '.join(VALID_CHOICES)}")
-        user_choice = input() 
-    
+        user_choice = input()
+
     computer_choice = random.choice(VALID_CHOICES)
-    
+
     prompt(f'You chose {user_choice}, the computer chose {computer_choice}')
     display_winner(user_choice, computer_choice)
-    
+
+    if (
+        (user_choice[0] == 'r' and computer_choice in ['lizard', 'scissors']) or
+        (user_choice[0] == 'p' and computer_choice in ['rock', 'spock']) or
+        (user_choice[0:2] == 'sc' and computer_choice in ['paper', 'lizard']) or
+        (user_choice[0:2] == 'sp' and computer_choice in ['rock', 'scissors']) or
+        (user_choice[0] == 'l' and computer_choice in ['paper', 'spock'])
+        ):
+        user_won += 1
+    elif (
+        (user_choice[0] == 'r' and computer_choice in ['paper', 'spock']) or
+        (user_choice[0] == 'p' and computer_choice in ['lizard', 'scissors']) or
+        (user_choice[0:2] == 'sc' and computer_choice in ['rock', 'spock']) or
+        (user_choice[0:2] == 'sp' and computer_choice in ['lizard', 'paper']) or
+        (user_choice[0] == 'l' and computer_choice in ['rock', 'scissors'])
+        ):
+        computer_won += 1
+
+    prompt(f'User Score: {user_won}/5')
+    prompt(f'Computer Score: {computer_won}/5')
+
+    if user_won == 3:
+        prompt("You won the game!")
+        break
+
+    if computer_won == 3:
+        prompt("You lost the game!")
+        break
+
     prompt('Play again (y/n)?')
     again = input().lower()
-    
+
     # while again[0] not in ['y', 'n']:
     while not (again.startswith('y') or again.startswith('n')):
         prompt('Invalid choice, please choose y/n')
         again = input().lower()
-        
+
     if again[0] == 'n':
         break
